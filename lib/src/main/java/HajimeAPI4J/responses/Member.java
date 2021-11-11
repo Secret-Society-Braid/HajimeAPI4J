f1032c33d9f53ddcdfaf51a227dab58969807a04
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2021-11-11 22:56:06 +0900

    doc: add javadoc

--- lib/src/main/java/HajimeAPI4J/responses/Member.java
+++ lib/src/main/java/HajimeAPI4J/responses/Member.java
@@ -1,5 +1,13 @@
 package HajimeAPI4J.responses;
 
+/**
+ * ふじわらはじめAPIで使用されているレスポンス型「Member」の宣言です。
+ * 他クラスと似通ったレスポンスパラメータを取るため、独自アダプターを継承しています。
+ * 
+ * @author Ranfa
+ * 
+ * @see ResponseAdapter
+ */
 public class Member extends ResponseAdapter {
 
 	private String name;
@@ -11,84 +19,129 @@ public class Member extends ResponseAdapter {
 	private String cv;
 	private boolean solo;
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Integer getTaxId() {
 		return taxId;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setTaxId(Integer taxId) {
 		this.taxId = taxId;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getLink() {
 		return link;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setLink(String link) {
 		this.link = link;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getApi() {
 		return api;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setApi(String api) {
 		this.api = api;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getProduction() {
 		return production;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setProduction(String production) {
 		this.production = production;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getCv() {
 		return cv;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setCv(String cv) {
 		this.cv = cv;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public boolean isSolo() {
 		return solo;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public void setSolo(boolean solo) {
 		this.solo = solo;
 	}
-
-
-
 }
--- lib/src/main/java/HajimeAPI4J/responses/Unit.java
+++ lib/src/main/java/HajimeAPI4J/responses/Unit.java
@@ -2,6 +2,14 @@ package HajimeAPI4J.responses;
 
 import java.util.List;
 
+/**
+ * ふじわらはじめAPIで使用されているレスポンス型「Unit」の宣言です。
+ * 似通ったレスポンスパラメータを取るため、独自アダプターを継承しています
+ * 
+ * @author Ranfa
+ * 
+ * @see ResponseAdapter
+ */
 public class Unit extends ResponseAdapter {
 
 	private String name;
@@ -11,61 +19,97 @@ public class Unit extends ResponseAdapter {
 	private String api;
 	private List<Member> members;
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getType() {
 		return type;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setType(String type) {
 		this.type = type;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Integer getTaxId() {
 		return taxId;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setTaxId(Integer taxId) {
 		this.taxId = taxId;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getLink() {
 		return link;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setLink(String link) {
 		this.link = link;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getApi() {
 		return api;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setApi(String api) {
 		this.api = api;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public List<Member> getMembers() {
 		return members;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void setMembers(List<Member> members) {
 		this.members = members;
