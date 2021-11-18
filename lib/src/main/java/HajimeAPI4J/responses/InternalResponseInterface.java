a008444a439ffb89d02e79a568e6f72a63cdd77c
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2021-11-18 21:20:39 +0900

    doc: add doc

--- lib/src/main/java/HajimeAPI4J/responses/InternalResponseInterface.java
+++ lib/src/main/java/HajimeAPI4J/responses/InternalResponseInterface.java
@@ -2,18 +2,55 @@ package HajimeAPI4J.responses;
 
 import javax.annotation.Nonnull;
 
+/**
+ * ふじわらはじめAPIで使用されているレスポンス型のインターフェースです。
+ * レスポンス型の宣言には {@link HajimeAPI4J.responses.ResponseAdapter アダプタークラス} を継承しています。
+ * 
+ * @author Ranfa
+ * @since 1.0.0
+ * 
+ * @see ResponseAdapter
+ * @see Unit
+ * @see Member
+ * @see Music
+ * @see Lyric
+ * @see Composer
+ * @see Arrange
+ * @see Disc
+ * @see Live
+ */
 public interface InternalResponseInterface {
 
+    /**
+     * レスポンスに含まれているパラメータ「date」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「date」であるもののvalue
+     */
     @Nonnull
     String getDate();
 
+    /**
+     * レスポンスに含まれているパラメータ「date」の値をセットします。
+     * @param date パラメータ「date」の値
+     */
     void setDate(@Nonnull String date);
 
+    /**
+     * レスポンスに含まれているパラメータ「production」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「production」であるもののvalue
+     */
     @Nonnull
     String getProduction();
 
+    /**
+     * レスポンスに含まれているパラメータ「production」の値をセットします。
+     * @param production パラメータ「production」の値
+     */
     void setProduction(@Nonnull String production);
 
+    /**
+     * レスポンスに含まれているパラメータ「kana」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「kana」であるもののvalue
+     */
     @Nonnull
     String getKana();
 
--- lib/src/main/java/HajimeAPI4J/responses/ResponseAdapter.java
+++ lib/src/main/java/HajimeAPI4J/responses/ResponseAdapter.java
@@ -28,96 +28,234 @@ public abstract class ResponseAdapter implements ResponseInterface,
                                                 InternalResponseInterface,
                                                 InternalArrayResponseInterface {
     
+    /**
+     * {@inheritDoc}
+     */
     public String getName() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setName(String name) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getType() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setType(@Nonnull String type) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public Integer getTaxId() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setTaxId(@Nonnull Integer taxId) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getLink() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setLink(@Nonnull String link) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getApi() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setApi(@Nonnull String api) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getDate() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setDate(@Nonnull String date) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getProduction() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setProduction(@Nonnull String production) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getCv() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setCv(@Nonnull String cv) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getKana() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setKana(@Nonnull String kana) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getCvKana() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setCvKana(@Nonnull String cvKana) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getPlace() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setPlace(@Nonnull String place) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public boolean getSetList() {return false;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setSetList(@Nonnull boolean setList) {}
     
+    /**
+     * {@inheritDoc}
+     */
     public Lyric getLyric() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setLyric(@Nonnull Lyric lyric) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public Composer getComposer() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setComposer(@Nonnull Composer composer) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public Arrange getArrange() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setArrange(@Nonnull Arrange arrange) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public String getLyricsUrl() {return null;}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setLyricsUrl(@Nonnull String lyricsUrl) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Member> getMembers() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setMembers(@Nonnull List<Member> members) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Music> getMusics() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setMusics(@Nonnull List<Music> musics) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Lyric> getLyrics() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setLyrics(@Nonnull List<Lyric> lyrics) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Composer> getComposers() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setComposers(@Nonnull List<Composer> composers) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Arrange> getArranges() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setArranges(@Nonnull List<Arrange> arranges) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Disc> getDisc() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setDisc(@Nonnull List<Disc> discs) {}
 
+    /**
+     * {@inheritDoc}
+     */
     public List<Live> getLives() {return Collections.emptyList();}
 
+    /**
+     * {@inheritDoc}
+     */
     public void setLives(@Nonnull List<Live> lives) {}
 
 }
