d9dc9e12aaf1a5b2bf5adb3209aa2593c0aafb0b
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2021-11-23 22:53:07 +0900

    doc: add all docs in internalResponseInterface

--- lib/src/main/java/HajimeAPI4J/responses/InternalResponseInterface.java
+++ lib/src/main/java/HajimeAPI4J/responses/InternalResponseInterface.java
@@ -54,46 +54,114 @@ public interface InternalResponseInterface {
     @Nonnull
     String getKana();
 
+    /**
+     * レスポンスに含まれているパラメータ「kana」の値をセットします。
+     * @param kana パラメータ「kana」の値
+     */
     void setKana(@Nonnull String kana);
 
+    /**
+     * レスポンスに含まれているパラメータ「cv」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「cv」であるもののvalue
+     */
     @Nonnull
     String getCv();
 
+    /**
+     * レスポンスに含まれているパラメータ「cv」の値をセットします。
+     * @param cv　パラメータ「cv」の値
+     */
     void setCv(@Nonnull String cv);
 
+    /**
+     * レスポンスに含まれているパラメータ「cvkana」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「cvkana」であるもののvalue
+     */
     @Nonnull
     String getCvKana();
 
+    /**
+     * レスポンスに含まれているパラメータ「cvkana」の値をセットします。
+     * @param cvKana パラメータ「cvkana」の値
+     */
     void setCvKana(@Nonnull String cvKana);
 
+    /**
+     * レスポンスに含まれているパラメータ「place」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち。keyが「place」であるもののvalue
+     */
     @Nonnull
     String getPlace();
 
+    /**
+     * レスポンスに含まれているパラメータ「place」の値をセットします。
+     * @param place パラメータ「place」の値
+     */
     void setPlace(@Nonnull String place);
 
+    /**
+     * レスポンスに含まれているパラメータ「setlist」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「setlist」であるもののvalue
+     */
     @Nonnull
     boolean getSetList();
 
+    /**
+     * レスポンスに含まれているパラメータ「setlist」の値をセットします。
+     * @param setList パラメータ「setlist」の値
+     */
     void setSetList(@Nonnull boolean setList);
 
+    /**
+     * レスポンスに含まれているパラメータ「lyric」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「lyric」であるもののvalue
+     */
     @Nonnull
     Lyric getLyric();
 
+    /**
+     * レスポンスに含まれているパラメータ「lyric」の値をセットします。
+     * @param lyric パラメータ「lyric」の値
+     */
     void setLyric(@Nonnull Lyric lyric);
 
+    /**
+     * レスポンスに含まれているパラメータ「composer」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「composer」であるもののvalue
+     */
     @Nonnull
     Composer getComposer();
 
+    /**
+     * レスポンスに含まれているパラメータ「composer」の値をセットします。
+     * @param composer パラメータ「composer」の値
+     */
     void setComposer(@Nonnull Composer composer);
 
+    /**
+     * レスポンスに含まれているパラメータ「arrange」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「arrange」であるもののvalue
+     */
     @Nonnull
     Arrange getArrange();
 
+    /**
+     * レスポンスに含まれているパラメータ「arrange」の値をセットします。
+     * @param arrange パラメータ「arrange」の値
+     */
     void setArrange(@Nonnull Arrange arrange);
 
+    /**
+     * レスポンスに含まれているパラメータ「lyricurl」の値を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「lyricurl」であるもののvalue
+     */
     @Nonnull
     String getLyricsUrl();
 
+    /**
+     * レスポンスに含まれているパラメータ「lyricurl」の値をセットします。
+     * @param lyricsUrl パラメータ「lyricurl」の値
+     */
     void setLyricsUrl(@Nonnull String lyricsUrl);
 
 }
