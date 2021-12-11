629f39906c0a92bcd4a5bd64511b6f2c1f2cdfb2
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2021-12-11 17:40:34 +0900

    doc: add doc

--- lib/src/main/java/HajimeAPI4J/responses/interfaces/InternalArrayResponseInterface.java
+++ lib/src/main/java/HajimeAPI4J/responses/interfaces/InternalArrayResponseInterface.java
@@ -45,37 +45,78 @@ public interface InternalArrayResponseInterface {
     void setMembers(@Nonnull List<Member> members);
 
     /**
-     * 
-     * @return
+     * レスポンスに含まれているパラメータ「music」の配列を返します。
+     * @return レスポンスに含まれているパラメータのうち、Keyが「music」であるものの配列
      */
     @Nonnull
     List<Music> getMusics();
 
+    /**
+     * レスポンスに含まれているパラメータ「music」の配列をセットします。
+     * @param musics パラメータ「music」の配列
+     */
     void setMusics(@Nonnull List<Music> musics);
 
+    /**
+     * レスポンスに含まれているパラメータ「lyrics」の配列を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「lyrics」であるものの配列
+     */
     @Nonnull
     List<Lyric> getLyrics();
 
+    /**
+     * レスポンスに含まれているパラメータ「lyrics」の配列をセットします。
+     * @param lyrics パラメータ「lyrics」の配列
+     */
     void setLyrics(@Nonnull List<Lyric> lyrics);
 
+    /**
+     * レスポンスに含まれているパラメータ「composer」の配列を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「composer」であるものの配列
+     */
     @Nonnull
     List<Composer> getComposers();
 
+    /**
+     * レスポンスに含まれているパラメータ「composer」の配列をセットします。
+     * @param composers パラメータ「composer」の配列
+     */
     void setComposers(@Nonnull List<Composer> composers);
-
+/**
+     * レスポンスに含まれているパラメータ「arrange」の配列を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「arrange」であるものの配列
+     */
     @Nonnull
     List<Arrange> getArranges();
 
+    /**
+     * レスポンスに含まれているパラメータ「arrange」の配列をセットします。
+     * @param arranges パラメータ「arrange」の配列
+     */
     void setArranges(@Nonnull List<Arrange> arranges);
-
+/**
+     * レスポンスに含まれているパラメータ「disc」の配列を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「disc」であるものの配列
+     */
     @Nonnull
     List<Disc> getDiscs();
 
+    /**
+     * レスポンスに含まれているパラメータ「disc」の配列をセットします。
+     * @param discs パラメータ「disc」の配列
+     */
     void setDiscs(@Nonnull List<Disc> discs);
-
+/**
+     * レスポンスに含まれているパラメータ「live」の配列を返します。
+     * @return レスポンスに含まれているパラメータのうち、keyが「live」であるものの配列
+     */
     @Nonnull
     List<Live> getLives();
 
+    /**
+     * レスポンスに含まれているパラメータ「live」の配列をセットします。
+     * @param lives パラメータ「live」の配列
+     */
     void setLives(@Nonnull List<Live> lives);
 
 }
