cf064897ce7521a95fee9a53d0dd799d7475c077
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2021-12-11 19:21:28 +0900

    refactor: add composer and lyric classes

--- lib/src/main/java/HajimeAPI4J/responses/Composer.java
+++ lib/src/main/java/HajimeAPI4J/responses/Composer.java
@@ -1,5 +1,102 @@
 package HajimeAPI4J.responses;
 
-public class Composer {
+import HajimeAPI4J.responses.interfaces.ResponseAdapter;
+
+/**
+ * ふじわらはじめAPIで使用されるレスポンス「composer」の宣言です。
+ * この宣言は {@link HajimeAPI4J.responses.interfaces.ResponseAdapter 独自アダプター} を継承しています。
+ */
+public class Composer extends ResponseAdapter {
+
+        // Declare field
+        private String name;
+        private String type;
+        private int taxId;
+        private String link;
+        private String api;
+        
+        // getter
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getName() {
+            return name;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getType() {
+            return type;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getTaxId() {
+            return taxId;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getLink() {
+            return link;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String getApi() {
+            return api;
+        }
+    
+        // setter
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void setType(String type) {
+            this.type = type;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void setTaxId(int taxId) {
+            this.taxId = taxId;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void setLink(String link) {
+            this.link = link;
+        }
+    
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void setApi(String api) {
+            this.api = api;
+        }
     
 }
