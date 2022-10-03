6567830e81ac693299e2a05fa15cda81089cb863
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-10-03 22:08:40 +0900

    add constructor

--- lib/src/main/java/hajimeapi4j/internal/MusicEndPointImpl.java
+++ lib/src/main/java/hajimeapi4j/internal/MusicEndPointImpl.java
@@ -1,11 +1,49 @@
 package hajimeapi4j.internal;
 
+import hajimeapi4j.api.endpoint.EndPoint;
 import hajimeapi4j.api.endpoint.MusicEndPoint;
+import hajimeapi4j.internal.datatype.Member;
+import hajimeapi4j.internal.datatype.utilizations.Disc;
+import hajimeapi4j.internal.datatype.utilizations.Live;
+import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class MusicEndPointImpl extends EndPointImpl implements MusicEndPoint {
+
+  protected List<EndPoint> remix;
+  protected List<EndPoint> original;
+  protected List<EndPoint> lyrics;
+  protected List<EndPoint> composer;
+  protected List<EndPoint> arrange;
+  protected String lyricsUrl;
+  protected List<Member> member;
+  protected boolean digital;
+  protected List<Disc> disc;
+  protected List<Live> live;
+
+  protected MusicEndPointImpl(
+      String name,
+      String type,
+      int taxId,
+      int songId,
+      String link,
+      String api,
+      String unit,
+      List<EndPoint> remix,
+      List<EndPoint> original,
+      List<EndPoint> lyrics,
+      List<EndPoint> composer,
+      List<EndPoint> arrange,
+      String lyricsUrl,
+      List<Member> member,
+      boolean digital,
+      List<Disc> disc,
+      List<Live> live) {
+    super(name, type, taxId, songId, link, api);
+
+  }
 
 }
