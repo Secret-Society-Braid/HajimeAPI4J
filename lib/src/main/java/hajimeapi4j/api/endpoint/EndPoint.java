3416edceed23b80a7478e2b0dbb7434ee7550af8
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-10-22 23:35:56 +0900

    doc: add whole document

--- lib/src/main/java/hajimeapi4j/api/endpoint/EndPoint.java
+++ lib/src/main/java/hajimeapi4j/api/endpoint/EndPoint.java
@@ -34,20 +34,47 @@ public interface EndPoint extends RestAction<EndPoint> {
 
   /**
    * ふじわらはじめAPIで規定されている、主に出力データタイプの規定に割り当てられているJSONキー「type」の情報を取得します。
+   *
    * @return JSONキー「type」に指定されている情報
    */
   @Nonnull
   String getType();
 
+  /**
+   * 空のインスタンスを作成します
+   *
+   * @return 空情報のインスタンス
+   */
+  @CheckReturnValue
+  static EndPoint createEmpty() {
+    return EndPointImpl.createEmpty();
+  }
+
+  /**
+   * ふじわらはじめAPIで規定されている、主にカテゴリID（ふじわらはじめAPI内部管理ID）に割り当てられているJSONキー「tax_id」の情報を取得します。
+   * <p>
+   * この情報は {@link #getSongId()}と排他的になっており、どちらか一方が必ず参照可能です。
+   *
+   * @return JSONキー「tax_id」に指定されている情報
+   */
   int getTaxId();
 
+  /**
+   * ふじわらはじめAPIで規定されている、主に楽曲ID（ふじわらはじめAPI内部管理ID）に割り当てられているJSONキー「song_id」の情報を取得します。
+   * <p>
+   * この情報は {@link #getTaxId()} と排他的になっており、どちらか一方が必ず参照可能です。
+   *
+   * @return JSONキー「song_id」に規定されている情報
+   */
   int getSongId();
 
+  /**
+   * ふじわらはじめAPIで規定されている、主にそのカテゴリページへのURLに割り当てられているJSONキー「link」の情報を取得します。
+   *
+   * @return JSONキー「link」に規定されている情報
+   */
   @Nonnull
   String getLink();
-
-  @Nonnull
-  String getApi();
 
   /**
    * 取得したJSON API情報を使用し、その情報の詳細を取得できるようにするためのユーティリティメソッドです。
@@ -58,10 +85,28 @@ public interface EndPoint extends RestAction<EndPoint> {
   @CheckReturnValue
   RestAction<EndPoint> fromApi();
 
-  boolean checkEmpty();
+  /**
+   * ふじわらはじめAPIで規定されている、主にそのカテゴリの詳細情報を格納しているAPIへのURI情報に割り当てられているJSONキー「api」の情報を取得します。
+   *
+   * @return JSONキー「api」に規定されている情報
+   */
+  @Nonnull
+  String getApi();
 
-  @CheckReturnValue
-  static EndPoint createEmpty() {
-    return EndPointImpl.createEmpty();
-  }
+  /**
+   * このインスタンスに格納されている情報が空であるか確認します。
+   * <p>
+   * 空である判定は
+   * <ul>
+   *   <li>{@link #getName()} が空文字列である</li>
+   *   <li>{@link #getType()} が空文字列である</li>
+   *   <li>{@link #getSongId()} もしくは {@link #getTaxId()} が{@code -1} である</li>
+   *   <li>{@link #getLink()} が空文字列である</li>
+   *   <li>{@link #getApi()} が空文字列である</li>
+   * </ul>
+   * 以上の条件で判定を行います。
+   *
+   * @return インスタンスが空である場合は {@code true}, そうでない場合は {@code false}
+   */
+  boolean checkEmpty();
 }
