0ea4ede4202420475abe551e782d1bbb7cd95371
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-10-26 19:25:39 +0900

    doc: add document for MusicEndPoint

--- lib/src/main/java/hajimeapi4j/api/endpoint/MusicEndPoint.java
+++ lib/src/main/java/hajimeapi4j/api/endpoint/MusicEndPoint.java
@@ -1,5 +1,7 @@
 package hajimeapi4j.api.endpoint;
 
+import hajimeapi4j.api.request.RestAction;
+import hajimeapi4j.internal.MusicEndPointImpl;
 import hajimeapi4j.internal.datatype.Member;
 import hajimeapi4j.internal.datatype.utilizations.Disc;
 import hajimeapi4j.internal.datatype.utilizations.Live;
@@ -12,30 +14,90 @@ import javax.annotation.Nonnull;
  * エンドポイント「/music」にて取得できる情報を定義したインターフェースです
  * <p>
  * {@code music}エンドポイントではエンドユーザーの指定によって出力される情報が変化するため、特に理由付けがなされていないかぎり{@link Optional}でラップされています。
+ *
+ * @author Ranfa
+ * @see RestAction
+ * @see EndPoint
+ * @see MusicEndPointImpl
+ * @since 5.0.0
  */
 public interface MusicEndPoint extends EndPoint {
 
+  /**
+   * 指定された楽曲IDがリミックス楽曲である場合に、原曲の情報を出力します。
+   * <p>
+   * その他のリミックス情報は出力しませんのでお気をつけください。
+   *
+   * @return リミックス元楽曲の、原曲情報
+   */
   @CheckReturnValue
   Optional<List<EndPoint>> getRemix();
 
+  /**
+   * 指定された楽曲IDがオリジナル楽曲である場合に、リミックスバージョン一覧を出力します。
+   * <p>
+   * {@link #getRemix()}メソッドとの混同にお気をつけください。
+   *
+   * @return リミックスバージョンの一覧
+   */
   @CheckReturnValue
   Optional<List<EndPoint>> getOriginal();
 
+  /**
+   * 作詞者の情報を出力します。
+   * <p>
+   * 1人での作業の場合も{@link List}形式となります。
+   *
+   * @return 楽曲に携わった作詞者情報
+   */
   @CheckReturnValue
   Optional<List<EndPoint>> getLyrics();
 
+  /**
+   * リアレンジ楽曲を含む、作曲者情報を出力します。
+   * <p>
+   * 一人での作業の場合も{@link List}形式となります。
+   *
+   * @return 楽曲に携わった作曲者情報
+   */
   @CheckReturnValue
   Optional<List<EndPoint>> getComposer();
 
+  /**
+   * リアレンジ楽曲のアレンジメント含む、編曲者情報を出力します。
+   * <p>
+   * 一人での作業の場合も{@link List}形式となります。
+   *
+   * @return 楽曲に携わった編曲者情報
+   */
   @CheckReturnValue
   Optional<List<EndPoint>> getArrange();
 
+  /**
+   * 歌詞が存在する曲について、歌詞サイトのURLを取得します。
+   * <p>
+   * リミックス楽曲では出力されないため、適宜原曲情報から参照する必要があります。
+   *
+   * @return 歌詞サイトへのリンクURL
+   */
   @CheckReturnValue
   Optional<String> getLyricsUrl();
 
+  /**
+   * CD、配信、ゲーム内参加メンバーを取得します。
+   * <p>
+   * この情報はnullになりえません。
+   *
+   * @return 参加メンバー一覧
+   */
   @Nonnull
   List<Member> getMember();
 
+  /**
+   * 指定された楽曲が、デジタル配信されているかどうかを取得します。
+   *
+   * @return デジタル配信が存在する場合は {@code true}、存在しない場合は{@code false}
+   */
   boolean getDigitalReleaseExists();
 
   /**
@@ -46,6 +108,11 @@ public interface MusicEndPoint extends EndPoint {
   @CheckReturnValue
   Optional<List<Disc>> getDisc();
 
+  /**
+   * 指定された楽曲が披露されたライブの情報を出力します。
+   *
+   * @return 披露された全てのライブの情報
+   */
   @CheckReturnValue
   Optional<List<Live>> getLive();
 
