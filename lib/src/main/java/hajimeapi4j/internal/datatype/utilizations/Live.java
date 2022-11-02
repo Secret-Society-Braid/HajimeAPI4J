6917480ab2783ad2bca99b4746d8482929106f05
hizumiaoba-tablet <hizumiaoba@gmail.com>
2022-11-02 13:30:49 +0900

    feat: add Live data class(WIP)

--- lib/src/main/java/hajimeapi4j/internal/datatype/utilizations/Live.java
+++ lib/src/main/java/hajimeapi4j/internal/datatype/utilizations/Live.java
@@ -1,5 +1,20 @@
 package hajimeapi4j.internal.datatype.utilizations;
 
-public class Live {
+import hajimeapi4j.api.endpoint.EndPoint;
+import hajimeapi4j.internal.EndPointImpl;
+import hajimeapi4j.internal.datatype.Member;
+import hajimeapi4j.internal.datatype.Unit;
+import java.util.List;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Live extends EndPointImpl implements EndPoint {
+
+  protected String date;
+  protected String place;
+  protected List<Unit> unit;
+  protected List<Member> member;
 
 }
