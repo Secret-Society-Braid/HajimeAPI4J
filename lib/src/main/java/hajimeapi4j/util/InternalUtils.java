220673f6f848c3aaaa4adc071816e3dd50b396c3
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-11-13 22:09:16 +0900

    add InternalUtils

--- lib/src/main/java/hajimeapi4j/util/InternalUtils.java
+++ lib/src/main/java/hajimeapi4j/util/InternalUtils.java
@@ -3,7 +3,11 @@ package hajimeapi4j.util;
 import com.fasterxml.jackson.databind.JsonNode;
 import hajimeapi4j.api.endpoint.EndPoint;
 import hajimeapi4j.api.endpoint.ListEndPoint;
+import hajimeapi4j.api.endpoint.TaxEndPoint;
 import hajimeapi4j.api.request.RestAction;
+import hajimeapi4j.internal.datatype.Member;
+import hajimeapi4j.internal.datatype.Unit;
+import hajimeapi4j.internal.datatype.utilizations.Music;
 import hajimeapi4j.internal.endpoint.ListEndPointImpl;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,6 +15,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
@@ -19,11 +24,16 @@ import lombok.extern.slf4j.Slf4j;
 public class InternalUtils {
 
   private static final String MESSAGE_METHOD_NOT_ALLOWED = "You cannot invoke this method from Lyrics class";
+  private static final String MESSAGE_NULL_RAW_RESPONSE_NOT_ALLOWED = "raw response must not be null";
 
   private InternalUtils() { /* do nothing */}
 
   public static String getMethodNotAllowedMessage() {
     return MESSAGE_METHOD_NOT_ALLOWED;
+  }
+
+  public static String getMessageNullRawResponseNotAllowedMessage() {
+    return MESSAGE_NULL_RAW_RESPONSE_NOT_ALLOWED;
   }
 
   public static boolean checkEmpty(EndPoint testee) {
@@ -67,7 +77,7 @@ public class InternalUtils {
     long start = System.currentTimeMillis();
     // check for potentially null access
     if (rawResponse == null) {
-      throw new IllegalArgumentException("raw response must not be null");
+      throw new IllegalArgumentException(getMessageNullRawResponseNotAllowedMessage());
     }
     log.debug("attempt to parse data");
     List<ListEndPoint> result = new ArrayList<>();
@@ -104,7 +114,49 @@ public class InternalUtils {
       log.debug("listEndPoint response instance has been created. : {}", eachInstance);
       result.add(eachInstance);
     }
-    log.debug("data parse completed. took {} ms", (System.currentTimeMillis() - start));
+    log.debug("list data parse completed. took {} ms", (System.currentTimeMillis() - start));
     return result;
+  }
+
+  @CheckReturnValue
+  public static TaxEndPoint generateTaxEndPointResponse(JsonNode rawResponse) {
+    long start = System.currentTimeMillis();
+    if (rawResponse == null) {
+      throw new IllegalArgumentException(getMessageNullRawResponseNotAllowedMessage());
+    }
+    log.debug("Attempt to parse tax data...");
+    // create some subclass instances
+    // there are "idol", "unit", "live", "disc" "lyrics", "arrange", "composer" types
+    final String type = rawResponse.get("type").asText();
+    EndPoint lyrics = null;
+    EndPoint composer = null;
+    EndPoint arrange = null;
+    List<Music> song = new ArrayList<>();
+    switch (type) {
+      case "idol":
+
+    }
+    JsonNode songNode = Objects.requireNonNull(rawResponse.get("song"));
+    for (JsonNode tmp : songNode) {
+      List<Unit> unit = new ArrayList<>();
+      List<Member> member = new ArrayList<>();
+      Music music = Music.createInstance(
+          tmp.get("name").asText(),
+          tmp.get("type").asText(),
+          tmp.get("music_type").asText(),
+          tmp.get("song_id").asInt(),
+          tmp.get("link").asText(),
+          tmp.get("api").asText(),
+          type.equals("live") ? tmp.get("song_text").asText() : null,
+          null, // TODO: parse unit data for tax data parsing
+          null, // TODO: parse member data for tax data parsing
+          type.equals("live") ? tmp.get("member_text").asText() : null,
+          type.equals("idol") ? tmp.get("solo").asBoolean() : null
+      );
+      log.debug("parse song data: {}", music);
+      song.add(music);
+    }
+    log.debug("tax data parsing complete. took {} ms", (System.currentTimeMillis() - start));
+    // return
   }
 }
