5390720081e6e6b173d7414a9691632c7abbd32d
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-11-17 22:38:16 +0900

    add prototype

--- lib/src/main/java/hajimeapi4j/util/SerializeTestDataClass.java
+++ lib/src/main/java/hajimeapi4j/util/SerializeTestDataClass.java
@@ -1,21 +1,43 @@
 package hajimeapi4j.util;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Optional;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
-@Getter
 @ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SerializeTestDataClass {
 
   private String name;
   private String type;
+  @Getter
   private int songId;
+  @Getter
   private int taxId;
   private String link;
   private String api;
+  private String nullValueTest;
 
+  public Optional<String> getName() {
+    return Optional.ofNullable(this.name);
+  }
+
+  public Optional<String> getType() {
+    return Optional.ofNullable(this.type);
+  }
+
+  public Optional<String> getLink() {
+    return Optional.ofNullable(this.link);
+  }
+
+  public Optional<String> getApi() {
+    return Optional.ofNullable(this.api);
+  }
+
+  public Optional<String> getNullValueTest() {
+    return Optional.ofNullable(this.nullValueTest);
+  }
 }
