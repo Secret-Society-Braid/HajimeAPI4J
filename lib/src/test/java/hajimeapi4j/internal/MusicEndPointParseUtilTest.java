aac1727232cc09b0c2365b0d6a49b17b6705497a
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-11-19 18:33:04 +0900

    test: add test objects

--- lib/src/test/java/hajimeapi4j/internal/MusicEndPointParseUtilTest.java
+++ lib/src/test/java/hajimeapi4j/internal/MusicEndPointParseUtilTest.java
@@ -1,14 +1,21 @@
 package hajimeapi4j.internal;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import hajimeapi4j.api.endpoint.EndPoint;
 import hajimeapi4j.api.endpoint.MusicEndPoint;
+import hajimeapi4j.internal.datatype.Member;
+import hajimeapi4j.internal.datatype.utilizations.Disc;
+import hajimeapi4j.internal.datatype.utilizations.Live;
 import hajimeapi4j.util.ParseUtil;
 import hajimeapi4j.util.TestingUtil;
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class MusicEndPointParseUtilTest {
@@ -27,6 +34,20 @@ class MusicEndPointParseUtilTest {
     // assertions
 
     assertNotNull(instance);
+
+    List<EndPoint> lyrics = instance.getLyrics().orElse(Collections.emptyList());
+    List<EndPoint> composer = instance.getComposer().orElse(Collections.emptyList());
+    List<EndPoint> arrange = instance.getArrange().orElse(Collections.emptyList());
+    List<Member> member = instance.getMember();
+    List<Disc> disc = instance.getDisc().orElse(Collections.emptyList());
+    List<Live> live = instance.getLive().orElse(Collections.emptyList());
+
+    assertFalse(lyrics.isEmpty());
+    assertFalse(composer.isEmpty());
+    assertFalse(arrange.isEmpty());
+    assertFalse(member.isEmpty());
+    assertFalse(disc.isEmpty());
+    assertFalse(live.isEmpty());
   }
 
 }
