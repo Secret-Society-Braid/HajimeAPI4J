c711cf745a96f4cfc31fc616f8210b4129dd75d7
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2022-11-20 22:06:43 +0900

    add checks

--- lib/src/main/java/hajimeapi4j/util/Checks.java
+++ lib/src/main/java/hajimeapi4j/util/Checks.java
@@ -1,6 +1,7 @@
 package hajimeapi4j.util;
 
 import com.google.common.base.Strings;
+import hajimeapi4j.exception.NoSuchParameterException;
 import java.util.Arrays;
 import java.util.List;
 import org.jetbrains.annotations.Contract;
@@ -37,8 +38,39 @@ public class Checks {
   }
 
   @Contract("null, _ -> fail")
-  public static void nonNullOrEmpty(String[] args,String varName) {
-    if(Arrays.stream(args).anyMatch(Strings::isNullOrEmpty))
+  public static void nonNullOrEmpty(String[] args, String varName) {
+    if (Arrays.stream(args).anyMatch(Strings::isNullOrEmpty)) {
       throw new IllegalArgumentException(String.format(NULL_OR_EMPTY_EXCEPTION_MESSAGE, varName));
+    }
+  }
+
+  @Contract("null -> fail")
+  public static void validateOrderParamString(String attempt) {
+    if (Strings.isNullOrEmpty(attempt)) {
+      throw new IllegalArgumentException(
+          String.format(NULL_OR_EMPTY_EXCEPTION_MESSAGE, "order parameter"));
+    }
+    if (!(attempt.equals("asc") || attempt.equals("desc"))) {
+      throw new NoSuchParameterException(
+          String.format(ILLEGAL_ARGS_EXCEPTION_MESSAGE, attempt, "order parameter"));
+    }
+  }
+
+  @Contract("null, _ -> fail")
+  public static void validateOrderByParamStringWithListEndPoint(String attempt,
+      boolean isTypeMusic) {
+    if (Strings.isNullOrEmpty(attempt)) {
+      throw new IllegalArgumentException(
+          String.format(NULL_OR_EMPTY_EXCEPTION_MESSAGE, "orderBy parameter"));
+    }
+    if (isTypeMusic) {
+      if (attempt.equals("title") || attempt.equals("date") || attempt.equals("rand")) {
+        return;
+      }
+    } else if (attempt.equals("name") || attempt.equals("count")) {
+      return;
+    }
+    throw new NoSuchParameterException(
+        String.format(ILLEGAL_ARGS_EXCEPTION_MESSAGE, attempt, "orderBy parameter"));
   }
 }
