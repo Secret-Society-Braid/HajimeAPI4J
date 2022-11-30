ea4b0d5db815ea591d3b520f92e8b6059704da0a
hizumiaoba-tablet <hizumiaoba@gmail.com>
2022-11-30 10:50:14 +0900

    delete unused util methods

--- lib/src/main/java/hajimeapi4j/util/Checks.java
+++ lib/src/main/java/hajimeapi4j/util/Checks.java
@@ -1,13 +1,7 @@
 package hajimeapi4j.util;
 
-import com.google.common.base.Strings;
-import hajimeapi4j.exception.NoSuchParameterException;
-import java.util.Arrays;
-import org.jetbrains.annotations.Contract;
-
 public class Checks {
 
-  private static final String NULL_OR_EMPTY_EXCEPTION_MESSAGE = "%s must not be null or empty";
   private static final String ILLEGAL_ARGS_EXCEPTION_MESSAGE = "%s is not applicable for %s endpoint";
 
   private Checks() { /* do nothing */ }
@@ -19,40 +13,4 @@ public class Checks {
     }
   }
 
-  @Contract("null, _ -> fail")
-  public static void nonNullOrEmpty(String[] args, String varName) {
-    if (Arrays.stream(args).anyMatch(Strings::isNullOrEmpty)) {
-      throw new IllegalArgumentException(String.format(NULL_OR_EMPTY_EXCEPTION_MESSAGE, varName));
-    }
-  }
-
-  @Contract("null -> fail")
-  public static void validateOrderParamString(String attempt) {
-    if (Strings.isNullOrEmpty(attempt)) {
-      throw new IllegalArgumentException(
-          String.format(NULL_OR_EMPTY_EXCEPTION_MESSAGE, "order parameter"));
-    }
-    if (!(attempt.equals("asc") || attempt.equals("desc"))) {
-      throw new NoSuchParameterException(
-          String.format(ILLEGAL_ARGS_EXCEPTION_MESSAGE, attempt, "order parameter"));
-    }
-  }
-
-  @Contract("null, _ -> fail")
-  public static void validateOrderByParamStringWithListEndPoint(String attempt,
-      boolean isTypeMusic) {
-    if (Strings.isNullOrEmpty(attempt)) {
-      throw new IllegalArgumentException(
-          String.format(NULL_OR_EMPTY_EXCEPTION_MESSAGE, "orderBy parameter"));
-    }
-    if (isTypeMusic) {
-      if (attempt.equals("title") || attempt.equals("date") || attempt.equals("rand")) {
-        return;
-      }
-    } else if (attempt.equals("name") || attempt.equals("count")) {
-      return;
-    }
-    throw new NoSuchParameterException(
-        String.format(ILLEGAL_ARGS_EXCEPTION_MESSAGE, attempt, "orderBy parameter"));
-  }
 }
