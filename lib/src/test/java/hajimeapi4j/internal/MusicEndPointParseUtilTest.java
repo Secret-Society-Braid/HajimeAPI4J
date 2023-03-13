725326b0ea7730e6ae4c1cafe1bdc1aa8d8b642e
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-03-13 13:11:31 +0900

    add more test cases for music endpoint

--- lib/src/test/java/hajimeapi4j/internal/MusicEndPointParseUtilTest.java
+++ lib/src/test/java/hajimeapi4j/internal/MusicEndPointParseUtilTest.java
@@ -1,21 +1,18 @@
 package hajimeapi4j.internal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import hajimeapi4j.api.endpoint.EndPoint;
 import hajimeapi4j.api.endpoint.MusicEndPoint;
-import hajimeapi4j.internal.datatype.Member;
-import hajimeapi4j.internal.datatype.utilizations.Disc;
-import hajimeapi4j.internal.datatype.utilizations.Live;
 import hajimeapi4j.util.ParseUtil;
 import hajimeapi4j.util.TestingUtil;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class MusicEndPointParseUtilTest {
@@ -35,19 +32,24 @@ class MusicEndPointParseUtilTest {
 
     assertNotNull(instance);
 
-    List<EndPoint> lyrics = instance.getLyrics().orElse(Collections.emptyList());
-    List<EndPoint> composer = instance.getComposer().orElse(Collections.emptyList());
-    List<EndPoint> arrange = instance.getArrange().orElse(Collections.emptyList());
-    List<Member> member = instance.getMember();
-    List<Disc> disc = instance.getDisc().orElse(Collections.emptyList());
-    List<Live> live = instance.getLive().orElse(Collections.emptyList());
+    // assertion for arrays
+    assertTrue(instance.getLyrics().isPresent());
+    assertTrue(instance.getComposer().isPresent());
+    assertTrue(instance.getArrange().isPresent());
+    assertTrue(instance.getDisc().isPresent());
+    assertTrue(instance.getLive().isPresent());
 
-    assertFalse(lyrics.isEmpty());
-    assertFalse(composer.isEmpty());
-    assertFalse(arrange.isEmpty());
-    assertFalse(member.isEmpty());
-    assertFalse(disc.isEmpty());
-    assertFalse(live.isEmpty());
+    assertFalse(instance.getMember().isEmpty());
+
+    // assertion for other fields
+    assertNotNull(instance.getName());
+    assertNotNull(instance.getType());
+    assertNotEquals(0, instance.getSongId());
+    assertEquals(0, instance.getTaxId());
+    assertNotNull(instance.getLink());
+    assertNotNull(instance.getApi());
+    assertNotNull(instance.getLyricsUrl());
+    assertTrue(instance.getDigitalReleaseExists());
   }
 
 }
