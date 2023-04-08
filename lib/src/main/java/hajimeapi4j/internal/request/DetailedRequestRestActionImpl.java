a110a3c2b4be8faf28bd4406421296425cc94b28
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-04-08 22:56:06 +0900

    add special class for directly create instance from api url string

---
+++ lib/src/main/java/hajimeapi4j/internal/request/DetailedRequestRestActionImpl.java
@@ -0,0 +1,21 @@
+package hajimeapi4j.internal.request;
+
+import javax.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DetailedRequestRestActionImpl<T> extends AbstractRestAction<T> {
+
+  private final String url;
+
+  public DetailedRequestRestActionImpl(@Nonnull String url, Class<T> clazz) {
+    super(clazz);
+    this.url = url;
+  }
+
+  @Nonnull
+  @Override
+  protected String constructUrl() {
+    return this.url;
+  }
+}
