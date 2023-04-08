5ebda450c281f1439577b90790bcb2c627f9f2b9
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-04-08 23:35:56 +0900

    fix for changing wrapper structure

--- lib/src/main/java/hajimeapi4j/internal/endpoint/AbstructEndPoint.java
+++ lib/src/main/java/hajimeapi4j/internal/endpoint/AbstructEndPoint.java
@@ -1,35 +1,27 @@
 package hajimeapi4j.internal.endpoint;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import hajimeapi4j.api.endpoint.EndPoint;
 import hajimeapi4j.api.request.RestAction;
+import hajimeapi4j.internal.request.DetailedRequestRestActionImpl;
 import hajimeapi4j.util.InternalUtils;
-import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
-/**
- * APIにおける、全てに共通しているパラメータ情報やリクエスト情報を定義します。
- * <p>
- * 一部パラメータ情報は情報が他パラメータ情報と共通している場合があります。ご注意ください。
- * </p>
- *
- * @author Ranfa
- * @see EndPoint
- * @since 5.0.0.0-Alpha.1
- */
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode
 @ToString
-@Setter
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class EndPointImpl implements EndPoint {
+@JsonDeserialize(as = EndPointImpl.class)
+public abstract class AbstructEndPoint implements EndPoint {
 
   protected String name;
   protected String type;
@@ -37,63 +29,45 @@ public class EndPointImpl implements EndPoint {
   protected int songId;
   protected String link;
   protected String api;
-  private static EndPoint emptyInstance;
 
   @Override
-  @Nonnull
   public String getName() {
     return this.name;
   }
 
   @Override
-  @Nonnull
   public String getType() {
     return this.type;
   }
 
   @Override
   public int getTaxId() {
-    return this.taxId;
+    return this.getSongId();
   }
 
   @Override
   public int getSongId() {
-    return this.songId;
+    return this.getTaxId();
   }
 
-  @Nonnull
   @Override
   public String getLink() {
     return this.link;
   }
 
-  @Nonnull
   @Override
   public String getApi() {
     return this.api;
   }
 
   @Override
-  public boolean checkEmpty() {
-    return InternalUtils.checkEmpty(this);
+  @Nonnull
+  public RestAction<EndPoint> fromApi() {
+    return new DetailedRequestRestActionImpl<>(this.getApi(), EndPoint.class);
   }
 
   @Override
-  @Nonnull
-  public RestAction<EndPoint> fromApi() {
-    return null;
-  }
-
-  /**
-   * 空の{@link EndPoint}インスタンスを返します。
-   *
-   * @return 空のインスタンス
-   */
-  @CheckReturnValue
-  public static EndPoint createEmpty() {
-    if (emptyInstance == null) {
-      emptyInstance = new EndPointImpl("", "", -1, -1, "", "");
-    }
-    return emptyInstance;
+  public boolean checkEmpty() {
+    return InternalUtils.checkEmpty(this);
   }
 }
