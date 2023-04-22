abb0fd38afa6f5cb9e7986ca968fda3c3eccc39d
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-04-22 22:07:35 +0900

    workaround fix for StackOverflow error when invoking #getSongId() or #getTaxId()

--- lib/src/main/java/hajimeapi4j/internal/endpoint/EndPointImpl.java
+++ lib/src/main/java/hajimeapi4j/internal/endpoint/EndPointImpl.java
@@ -41,6 +41,30 @@ public class EndPointImpl extends AbstructEndPoint {
   protected String api;
   private static EndPoint emptyInstance;
 
+  /**
+   * ふじわらはじめAPIで規定されている、主にカテゴリID（ふじわらはじめAPI内部管理ID）に割り当てられているJSONキー「tax_id」の情報を取得します。
+   * <p>
+   * この情報は {@link #getSongId()}と排他的になっており、どちらか一方が必ず参照可能です。
+   *
+   * @return JSONキー「tax_id」に指定されている情報
+   */
+  @Override
+  public int getTaxId() {
+    return this.taxId != 0 ? this.taxId : super.getTaxId();
+  }
+
+  /**
+   * ふじわらはじめAPIで規定されている、主に楽曲ID（ふじわらはじめAPI内部管理ID）に割り当てられているJSONキー「song_id」の情報を取得します。
+   * <p>
+   * この情報は {@link #getTaxId()} と排他的になっており、どちらか一方が必ず参照可能です。
+   *
+   * @return JSONキー「song_id」に規定されている情報
+   */
+  @Override
+  public int getSongId() {
+    return this.songId != 0 ? this.songId : super.getSongId();
+  }
+
   @Override
   @Nonnull
   public String getName() {
