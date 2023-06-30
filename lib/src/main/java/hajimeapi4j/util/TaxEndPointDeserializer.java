77e04c2c5c00970b1cc24f01e72c965be7966958
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-06-30 15:09:15 +0900

    fix custom deserializer for match v3v demand

--- lib/src/main/java/hajimeapi4j/util/TaxEndPointDeserializer.java
+++ lib/src/main/java/hajimeapi4j/util/TaxEndPointDeserializer.java
@@ -19,22 +19,22 @@ public class TaxEndPointDeserializer extends JsonDeserializer<TaxEndPoint> {
 
   @Override
   public TaxEndPoint deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-    JsonNode node = p.readValueAsTree();
+    JsonNode node = ctxt.readTree(p);
     if (!node.hasNonNull("member")) {
       return p.readValueAs(TaxEndPoint.class);
     }
     String name = node.get("name").asText();
     String type = node.get("type").asText();
-    int id = node.get("id").asInt();
+    int id = node.get("tax_id").asInt();
     String link = node.get("link").asText();
     String api = node.get("api").asText();
     String kana = node.get("kana").asText();
-    String cv = node.get("cv").asText();
-    String cvKana = node.get("cvkana").asText();
-    String production = node.get("production").asText();
-    String date = node.get("date").asText();
-    String place = node.get("place").asText();
-    boolean setlist = node.get("setlist").asBoolean();
+    String cv = node.hasNonNull("cv") ? node.get("cv").asText() : null;
+    String cvKana = node.hasNonNull("cvkana") ? node.get("cvkana").asText() : null;
+    String production = node.hasNonNull("production") ? node.get("production").asText() : null;
+    String date = node.hasNonNull("date") ? node.get("date").asText() : null;
+    String place = node.hasNonNull("place") ? node.get("place").asText() : null;
+    Boolean setlist = node.hasNonNull("setlist") ? node.get("setlist").asBoolean() : null;
     EndPoint lyrics = node.get("lyrics").traverse().readValueAs(EndPoint.class);
     EndPoint composer = node.get("composer").traverse().readValueAs(EndPoint.class);
     EndPoint arrange = node.get("arrange").traverse().readValueAs(EndPoint.class);
@@ -52,6 +52,6 @@ public class TaxEndPointDeserializer extends JsonDeserializer<TaxEndPoint> {
       member = memberName.parallelStream().map(Member::createInstance).collect(Collectors.toList());
     }
     return TaxEndPointImpl.createInstance(name, type, id, link, api, kana, cv, cvKana, production,
-        date, place, member, setlist, lyrics, composer, arrange, song);
+        date, place, member, Boolean.TRUE.equals(setlist), lyrics, composer, arrange, song);
   }
 }
