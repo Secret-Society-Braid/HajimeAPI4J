431151d1a4c7a6945c3491d435f6187be12c2908
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-04 17:50:43 +0900

    add tests

--- lib/src/test/java/hajimeapi4j/internal/TaxEndPointParseUtilTest.java
+++ lib/src/test/java/hajimeapi4j/internal/TaxEndPointParseUtilTest.java
@@ -1,12 +1,5 @@
 package hajimeapi4j.internal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hajimeapi4j.api.endpoint.TaxEndPoint;
@@ -14,12 +7,15 @@ import hajimeapi4j.internal.datatype.Member;
 import hajimeapi4j.internal.datatype.utilizations.Song;
 import hajimeapi4j.util.ParseUtil;
 import hajimeapi4j.util.TestingUtil;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class TaxEndPointParseUtilTest {
 
@@ -80,5 +76,27 @@ class TaxEndPointParseUtilTest {
   void plainMemberResponseTest() {
     ObjectMapper mapper = TestingUtil.getMapperInstance();
     TaxEndPoint instance = null;
+
+    try {
+      instance = mapper.readValue(TaxEndPointParseUtilTest.class.getResourceAsStream("/dataClassTemplate/v3/tax/plainMemberUnitResponse.json"), TaxEndPoint.class);
+    } catch (IOException e) {
+      fail(e);
+    }
+
+    // assertions
+    assertNotNull(instance);
+    log.debug(instance.toString());
+
+    assertNotNull(instance.getName());
+    assertNotNull(instance.getType());
+    assertNotEquals(0, instance.getTaxId());
+    assertNotNull(instance.getLink());
+    assertNotNull(instance.getApi());
+
+    assertEquals(5, instance.getSong().size());
+    instance.getMember().orElse(Collections.emptyList()).forEach(member -> {
+      assertNotNull(member.getName());
+      assertEquals(-1, member.getTaxId());
+    });
   }
 }
