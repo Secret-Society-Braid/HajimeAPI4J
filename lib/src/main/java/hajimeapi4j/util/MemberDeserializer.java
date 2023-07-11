fba54a6f9ac878c6f718bb723a439b16fe0665d2
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-07-12 08:42:11 +0900

    fix: change logic when only member name was provided.

--- lib/src/main/java/hajimeapi4j/util/MemberDeserializer.java
+++ lib/src/main/java/hajimeapi4j/util/MemberDeserializer.java
@@ -9,8 +9,11 @@ import hajimeapi4j.internal.datatype.Member;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 
 @Slf4j
 public class MemberDeserializer extends StdDeserializer<List<Member>> {
@@ -30,10 +33,82 @@ public class MemberDeserializer extends StdDeserializer<List<Member>> {
     } else if (p.getCurrentToken() == JsonToken.VALUE_STRING) {
       return Arrays.stream(p.getValueAsString().split(","))
           .map(String::trim)
-          .map(Member::createInstance)
+          .map(MinimalMember::new)
           .collect(Collectors.toList());
     } else {
       throw new IllegalStateException("Unexpected token: " + p.getCurrentToken());
+    }
+  }
+
+  static class MinimalMember extends Member {
+
+    public MinimalMember(String name) {
+      super(name, null, 0, 0, null, null, null, null);
+    }
+
+    /**
+     * 名前を取得します。
+     *
+     * @return 名前
+     */
+    @Override
+    @Nonnull
+    public String getName() {
+      return super.getName();
+    }
+
+    @Override
+    @Nonnull
+    public String getType() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    @Override
+    public int getTaxId() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    @Override
+    public int getSongId() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    @NotNull
+    @Override
+    public String getLink() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    @Override
+    @Nonnull
+    public String getApi() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    /**
+     * このアイドルの所属プロダクションを返します。
+     *
+     * @return このアイドルの所属プロダクション
+     */
+    @Override
+    public Optional<String> getProduction() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    /**
+     * このメンバーの声優を返します。
+     *
+     * @return 声優
+     */
+    @NotNull
+    @Override
+    public String getCv() {
+      throw new UnsupportedOperationException("Not Supported for your purpose.");
+    }
+
+    @Override
+    public String toString() {
+      return String.format("MinimalMember(Member[name=%s])", getName());
     }
   }
 }
